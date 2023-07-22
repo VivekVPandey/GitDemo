219361347f0e631751ad63016dbed3e9ccf1d93e
Vivek <vivekvpandey01@gmail.com>
2023-07-22 22:31:00 +0530

    my first commit

--- src/test/java/RestInter.java
+++ src/test/java/RestInter.java
@@ -3,6 +3,7 @@ public class RestInter {
 
 	public static void main(String[] args) {
 		System.out.println("Lets practice and crack the BCI modulr interview");
+		// lets practice git and get hands on for pull push n all
 	}
 	
 }
